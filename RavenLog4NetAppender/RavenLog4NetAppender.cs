30fc00e0f4d30d725513b96e390c23cbed002f58
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-09-27 14:41:13 +0200

    Added the Translate method to translate from log4net's 'Level' to raven-csharp's 'ErrorLevel'.

--- RavenLog4NetAppender/RavenLog4NetAppender.cs
+++ RavenLog4NetAppender/RavenLog4NetAppender.cs
@@ -1,50 +1,62 @@
 ﻿using System.Collections.Generic;
+
+using SharpRaven;
+using SharpRaven.Data;
+
 using log4net.Appender;
 using log4net.Core;
-using SharpRaven;
 
 namespace RavenLog4NetAppender
 {
-	public class RavenLog4NetAppender : AppenderSkeleton
-	{
-		private static RavenClient ravenClient;
+    public class RavenLog4NetAppender : AppenderSkeleton
+    {
+        private static RavenClient ravenClient;
+        public string DSN { get; set; }
+        public string Logger { get; set; }
 
-		protected override void Append(LoggingEvent loggingEvent)
-		{
-			if (ravenClient == null)
-			{
-				ravenClient = new RavenClient(DSN);
-				ravenClient.Logger = Logger;
-			}
 
-			if (loggingEvent.ExceptionObject != null)
-			{
-				ravenClient.CaptureException(loggingEvent.ExceptionObject);
-			}
-			else
-			{
-				// TODO: Handle log4net messages without an exception.
-				var data = loggingEvent.MessageObject as IList<string>;
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            if (ravenClient == null)
+            {
+                ravenClient = new RavenClient(DSN);
+                ravenClient.Logger = Logger;
+            }
 
-				if (data != null)
-				{
-					foreach (string s in data)
-					{
-						// Do something with each string
-					}
-				}
-			}
-		}
+            if (loggingEvent.ExceptionObject != null)
+            {
+                ravenClient.CaptureException(loggingEvent.ExceptionObject);
+            }
+            else
+            {
+                var level = Translate(loggingEvent.Level);
+                // TODO: Handle log4net messages without an exception.
+                var data = loggingEvent.MessageObject as IList<string>;
 
-		protected override void Append(LoggingEvent[] loggingEvents)
-		{
-			foreach (var loggingEvent in loggingEvents)
-			{
-				Append(loggingEvent);
-			}
-		}
+                if (data != null)
+                {
+                    foreach (string s in data)
+                    {
+                        // Do something with each string
+                        ravenClient.CaptureMessage(s, level);
+                    }
+                }
+            }
+        }
 
-		public string DSN { get; set; }
-		public string Logger { get; set; }
-	} 
+
+        internal static ErrorLevel Translate(Level level)
+        {
+            return ErrorLevel.debug;
+        }
+
+
+        protected override void Append(LoggingEvent[] loggingEvents)
+        {
+            foreach (var loggingEvent in loggingEvents)
+            {
+                Append(loggingEvent);
+            }
+        }
+    }
 }
