9f68cb2a04e401705e6b0acf5eb7ac2ab0aa9fee
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-09-27 14:39:04 +0200

    Added log4net and raven-csharp NuGet references to RavenLog4NetAppenderTests and a simple ErrorLevelTests class

---
+++ RavenLog4NetAppenderTests/ErrorLevelTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+using SharpRaven.Data;
+
+using log4net.Core;
+
+
+namespace RavenLog4NetAppenderTests
+{
+    [TestFixture]
+    public class ErrorLevelTests
+    {
+        [Test]
+        public void Translate_UnknownLevel_ReturnsError()
+        {
+            var level = RavenLog4NetAppender.RavenLog4NetAppender.Translate(Level.Alert);
+            Assert.That(level, Is.EqualTo(ErrorLevel.error));
+        }
+    }
+}
