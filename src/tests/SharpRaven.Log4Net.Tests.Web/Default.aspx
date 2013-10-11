9ac6bf1a783d929e8588e300a3f531bb9cb7e277
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2013-10-11 09:58:27 +0200

    Added web application to test the capture of web context metadata

---
+++ src/tests/SharpRaven.Log4Net.Tests.Web/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" %>
+<%@ Import Namespace="log4net" %>
+<!DOCTYPE html>
+<script runat="server">
+
+
+    private void DivideByZero(int stackFrames = 10)
+    {
+        if (stackFrames == 0)
+        {
+            var a = 0;
+            var b = 1 / a;
+        }
+        else
+            DivideByZero(--stackFrames);
+    }
+
+
+    private void Page_Load(object sender, EventArgs e)
+    {
+        var log = LogManager.GetLogger(GetType());
+
+        try
+        {
+            DivideByZero();
+        }
+        catch (Exception exception)
+        {
+            log.Fatal("Unexpected Exception in Web Application", exception);
+        }
+    }
+
+
+</script>
+<html>
+    <head runat="server">
+        <title>SharpRaven.Log4Net.Tests.Web</title>
+    </head>
+    <body>
+        <h1>Exception captured!</h1>
+    </body>
+</html>
---
+++ src/tests/SharpRaven.Log4Net.Tests.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SharpRaven.Log4Net.Tests.Web")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SharpRaven.Log4Net.Tests.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3b3668ab-28ec-4f48-af32-08b98862b163")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
