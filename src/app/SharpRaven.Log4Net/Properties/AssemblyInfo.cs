f6e32c3bba8d4ef85bd77006777bb1f795ab0bad
Chris Eldredge <chris.eldredge@gmail.com>
2014-07-02 18:44:54 +0100

    Use Ciao for continuous integration.

--- src/app/SharpRaven.Log4Net/Properties/AssemblyInfo.cs
+++ src/app/SharpRaven.Log4Net/Properties/AssemblyInfo.cs
@@ -22,16 +22,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("cb991214-41eb-463a-bce4-35cff33e9200")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+#if DEBUG
 [assembly: InternalsVisibleTo("SharpRaven.Log4Net.Tests")]
+#endif
--- src/tests/SharpRaven.Log4Net.Tests.Web/Properties/AssemblyInfo.cs
+++ src/tests/SharpRaven.Log4Net.Tests.Web/Properties/AssemblyInfo.cs
@@ -21,15 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3b3668ab-28ec-4f48-af32-08b98862b163")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
